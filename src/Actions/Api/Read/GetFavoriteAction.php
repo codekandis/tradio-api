78d88bfc95c1f52009b5e35cd4dec4c8ce0860cf
Christian Ramelow <info@codekandis.net>
2019-08-05 18:26:50 +0200

    Adds the `GetFavoriteAction`.

---
+++ src/Actions/Api/Read/GetFavoriteAction.php
@@ -0,0 +1,101 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TradioApi\Actions\Api\Read;
+
+use CodeKandis\Tiphy\Actions\AbstractAction;
+use CodeKandis\Tiphy\Http\Responses\JsonResponder;
+use CodeKandis\Tiphy\Http\Responses\StatusCodes;
+use CodeKandis\Tiphy\Persistence\MariaDb\Connector;
+use CodeKandis\Tiphy\Persistence\MariaDb\ConnectorInterface;
+use CodeKandis\Tiphy\Persistence\PersistenceException;
+use CodeKandis\TradioApi\Configurations\ConfigurationRegistry;
+use CodeKandis\TradioApi\Entities\FavoriteEntity;
+use CodeKandis\TradioApi\Entities\UriExtenders\FavoriteUriExtender;
+use CodeKandis\TradioApi\Http\UriBuilders\ApiUriBuilder;
+use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\FavoritesRepository;
+use ReflectionException;
+
+class GetFavoriteAction extends AbstractAction
+{
+	/** @var ConnectorInterface */
+	private $databaseConnector;
+
+	/** @var ApiUriBuilder */
+	private $uriBuilder;
+
+	private function getDatabaseConnector(): ConnectorInterface
+	{
+		if ( null === $this->databaseConnector )
+		{
+			$databaseConfig          = ConfigurationRegistry::_()->getPersistenceConfiguration();
+			$this->databaseConnector = new Connector( $databaseConfig );
+		}
+
+		return $this->databaseConnector;
+	}
+
+	private function getUriBuilder(): ApiUriBuilder
+	{
+		if ( null === $this->uriBuilder )
+		{
+			$uriBuilderConfiguration = ConfigurationRegistry::_()->getUriBuilderConfiguration();
+			$this->uriBuilder        = new ApiUriBuilder( $uriBuilderConfiguration );
+		}
+
+		return $this->uriBuilder;
+	}
+
+	/**
+	 * @throws PersistenceException
+	 * @throws ReflectionException
+	 */
+	public function execute(): void
+	{
+		$inputData = $this->getInputData();
+
+		$requestedFavorite     = new FavoriteEntity();
+		$requestedFavorite->id = $inputData[ 'id' ];
+		$favorite              = $this->readFavorite( $requestedFavorite );
+
+		if ( null === $favorite )
+		{
+			( new JsonResponder( StatusCodes::NOT_FOUND, null ) )
+				->respond();
+
+			return;
+		}
+
+		$this->extendUris( $favorite );
+
+		$responderData = [
+			'favorite' => $favorite
+		];
+		( new JsonResponder( StatusCodes::OK, $responderData ) )
+			->respond();
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getInputData(): array
+	{
+		return $this->arguments;
+	}
+
+	private function extendUris( FavoriteEntity $favorite ): void
+	{
+		$uriBuilder = $this->getUriBuilder();
+		( new FavoriteUriExtender( $uriBuilder, $favorite ) )
+			->extend();
+	}
+
+	/**
+	 * @throws PersistenceException
+	 */
+	private function readFavorite( FavoriteEntity $requestedFavorite ): ?FavoriteEntity
+	{
+		$databaseConnector = $this->getDatabaseConnector();
+
+		return ( new FavoritesRepository( $databaseConnector ) )
+			->readFavoriteById( $requestedFavorite );
+	}
+}
