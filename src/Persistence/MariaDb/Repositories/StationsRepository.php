f16fe4f3431935f5338368e1d41a342678f92df9
Christian Ramelow <info@codekandis.net>
2019-08-05 17:59:48 +0200

    Adds the missing methods to the `StationsRepository`.

--- src/Persistence/MariaDb/Repositories/StationsRepository.php
+++ src/Persistence/MariaDb/Repositories/StationsRepository.php
@@ -3,7 +3,9 @@ namespace CodeKandis\TradioApi\Persistence\MariaDb\Repositories;
 
 use CodeKandis\Tiphy\Persistence\MariaDb\Repositories\AbstractRepository;
 use CodeKandis\Tiphy\Persistence\PersistenceException;
+use CodeKandis\TradioApi\Entities\FavoriteEntity;
 use CodeKandis\TradioApi\Entities\StationEntity;
+use CodeKandis\TradioApi\Entities\UserEntity;
 
 class StationsRepository extends AbstractRepository
 {
@@ -72,5 +74,87 @@ class StationsRepository extends AbstractRepository
 		}
 
 		return $result;
+	}
+
+	/**
+	 * @return StationEntity[]
+	 * @throws PersistenceException
+	 */
+	public function readStationsByUserId( UserEntity $user ): array
+	{
+		$query = <<< END
+			SELECT
+				`stations`.*
+			FROM
+				`stations`
+			INNER JOIN
+				`users_stations`
+				ON
+				`users_stations`.`userId` = :userId
+			WHERE
+				`stations`.`id` = `users_stations`.`stationId`
+			ORDER BY
+			    `stations`.`name` ASC;
+		END;
+
+		$arguments = [
+			'userId' => $user->id
+		];
+
+		try
+		{
+			$this->databaseConnector->beginTransaction();
+			/** @var StationEntity[] $resultSet */
+			$resultSet = $this->databaseConnector->queryPrepared( $query, $arguments, StationEntity::class );
+			$this->databaseConnector->commit();
+		}
+		catch ( PersistenceException $exception )
+		{
+			$this->databaseConnector->rollback();
+			throw $exception;
+		}
+
+		return $resultSet;
+	}
+
+	/**
+	 * @return StationEntity[]
+	 * @throws PersistenceException
+	 */
+	public function readStationsByFavoriteId( FavoriteEntity $favorite ): array
+	{
+		$query = <<< END
+			SELECT
+				`stations`.*
+			FROM
+				`stations`
+			INNER JOIN
+				`stations_favorites`
+				ON
+				`stations_favorites`.`favoriteId` = :favoriteId
+			WHERE
+				`stations`.`id` = `stations_favorites`.`stationId`
+			ORDER BY
+			    `stations`.`name` ASC;
+		END;
+
+		$arguments = [
+			'favoriteId' => $favorite->id
+		];
+
+		try
+		{
+			$this->databaseConnector->beginTransaction();
+			/** @var StationEntity[] $resultSet */
+			$resultSet = $this->databaseConnector->queryPrepared( $query, $arguments, StationEntity::class );
+			$this->databaseConnector->commit();
+		}
+		catch ( PersistenceException $exception )
+		{
+			$this->databaseConnector->rollback();
+			throw $exception;
+		}
+
+		return $resultSet;
 	}
 }
