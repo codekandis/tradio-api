c4869752ef1cd17ea65f644a4eb37a14db185511
Christian Ramelow <info@codekandis.net>
2019-08-05 18:26:11 +0200

    Adds the `GetUserStationsAction`.

---
+++ src/Actions/Api/Read/GetUserStationsAction.php
@@ -0,0 +1,122 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TradioApi\Actions\Api\Read;
+
+use CodeKandis\Tiphy\Actions\AbstractAction;
+use CodeKandis\Tiphy\Http\Responses\JsonResponder;
+use CodeKandis\Tiphy\Http\Responses\StatusCodes;
+use CodeKandis\Tiphy\Persistence\MariaDb\Connector;
+use CodeKandis\Tiphy\Persistence\MariaDb\ConnectorInterface;
+use CodeKandis\Tiphy\Persistence\PersistenceException;
+use CodeKandis\TradioApi\Configurations\ConfigurationRegistry;
+use CodeKandis\TradioApi\Entities\StationEntity;
+use CodeKandis\TradioApi\Entities\UriExtenders\StationUriExtender;
+use CodeKandis\TradioApi\Entities\UserEntity;
+use CodeKandis\TradioApi\Http\UriBuilders\ApiUriBuilder;
+use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\StationsRepository;
+use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\UsersRepository;
+use ReflectionException;
+
+class GetUserStationsAction extends AbstractAction
+{
+	/** @var ConnectorInterface */
+	private $databaseConnector;
+
+	/** @var ApiUriBuilder */
+	private $uriBuilder;
+
+	private function getDatabaseConnector(): ConnectorInterface
+	{
+		if ( null === $this->databaseConnector )
+		{
+			$databaseConfig          = ConfigurationRegistry::_()->getPersistenceConfiguration();
+			$this->databaseConnector = new Connector( $databaseConfig );
+		}
+
+		return $this->databaseConnector;
+	}
+
+	private function getUriBuilder(): ApiUriBuilder
+	{
+		if ( null === $this->uriBuilder )
+		{
+			$uriBuilderConfiguration = ConfigurationRegistry::_()->getUriBuilderConfiguration();
+			$this->uriBuilder        = new ApiUriBuilder( $uriBuilderConfiguration );
+		}
+
+		return $this->uriBuilder;
+	}
+
+	/**
+	 * @throws PersistenceException
+	 * @throws ReflectionException
+	 */
+	public function execute(): void
+	{
+		$inputData = $this->getInputData();
+
+		$requestedUser     = new UserEntity();
+		$requestedUser->id = $inputData[ 'id' ];
+		$user              = $this->readUser( $requestedUser );
+
+		if ( null === $user )
+		{
+			( new JsonResponder( StatusCodes::NOT_FOUND, null ) )
+				->respond();
+
+			return;
+		}
+
+		$stations = $this->readUsersStations( $user );
+		$this->extendUris( $stations );
+
+		$responderData = [
+			'stations' => $stations,
+		];
+		( new JsonResponder( StatusCodes::OK, $responderData ) )
+			->respond();
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getInputData(): array
+	{
+		return $this->arguments;
+	}
+
+	/**
+	 * @param StationEntity[] $stations
+	 */
+	private function extendUris( array $stations ): void
+	{
+		$uriBuilder = $this->getUriBuilder();
+		foreach ( $stations as $station )
+		{
+			( new StationUriExtender( $uriBuilder, $station ) )
+				->extend();
+		}
+	}
+
+	/**
+	 * @throws PersistenceException
+	 */
+	private function readUser( UserEntity $requestedUser ): ?UserEntity
+	{
+		$databaseConnector = $this->getDatabaseConnector();
+
+		return ( new UsersRepository( $databaseConnector ) )
+			->readUserById( $requestedUser );
+	}
+
+	/**
+	 * @return StationEntity[]
+	 * @throws PersistenceException
+	 */
+	private function readUsersStations( UserEntity $user ): array
+	{
+		$databaseConnector = $this->getDatabaseConnector();
+
+		return ( new StationsRepository( $databaseConnector ) )
+			->readStationsByUserId( $user );
+	}
+}
