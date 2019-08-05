ff88fcbeba7bfe81f0effd071c8cd3329adea9ce
Christian Ramelow <info@codekandis.net>
2019-08-05 17:55:26 +0200

    Refactors and adds all necessary routes to the routes configuration.

--- config/routes.php
+++ config/routes.php
@@ -6,15 +6,45 @@ use CodeKandis\TradioApi\Actions\Api;
 
 return [
 	'^/$'                                                                                    => [
-		Methods::GET => Api\Read\GetIndexAction::class,
+		Methods::GET => Api\Read\GetIndexAction::class
 	],
 	'^/stations$'                                                                            => [
-		Methods::GET => Api\Read\GetStationsAction::class,
+		Methods::GET => Api\Read\GetStationsAction::class
 	],
 	'^/stations/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})$'                         => [
-		Methods::GET => Api\Read\GetStationAction::class,
+		Methods::GET => Api\Read\GetStationAction::class
 	],
 	'^/stations/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/tracklist/current-track$' => [
-		Methods::GET => Api\Read\GetCurrentTrackAction::class,
+		Methods::GET => Api\Read\GetCurrentTrackAction::class
 	],
+	'^/stations/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/users$'                   => [
+		Methods::GET => Api\Read\GetStationUsersAction::class
+	],
+	'^/stations/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/favorites$'               => [
+		Methods::GET => Api\Read\GetStationFavoritesAction::class
+	],
+	'^/users$'                                                                               => [
+		Methods::GET => Api\Read\GetUsersAction::class
+	],
+	'^/users/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})$'                            => [
+		Methods::GET => Api\Read\GetUserAction::class
+	],
+	'^/users/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/stations$'                   => [
+		Methods::GET => Api\Read\GetUserStationsAction::class
+	],
+	'^/users/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/favorites$'                  => [
+		Methods::GET => Api\Read\GetUserFavoritesAction::class
+	],
+	'^/favorites$'                                                                           => [
+		Methods::GET => Api\Read\GetFavoritesAction::class
+	],
+	'^/favorites/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})$'                        => [
+		Methods::GET => Api\Read\GetFavoriteAction::class
+	],
+	'^/favorites/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/stations$'               => [
+		Methods::GET => Api\Read\GetFavoriteStationsAction::class
+	],
+	'^/favorites/(?<id>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/users$'                  => [
+		Methods::GET => Api\Read\GetFavoriteUsersAction::class
+	]
 ];
