341563d4a78ad90f60452f33a2f27080f91dc80c
Christian Ramelow <info@codekandis.net>
2019-08-14 14:37:47 +0200

    Adds the `StationsErrorMessages`.

---
+++ src/Errors/StationsErrorMessages.php
@@ -0,0 +1,7 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TradioApi\Errors;
+
+abstract class StationsErrorMessages
+{
+	public const STATION_UNKNOWN = 'The requested station does not exist.';
+}
