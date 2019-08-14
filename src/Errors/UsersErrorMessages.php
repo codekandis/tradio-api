3a37567f5c2a3567d53249ff6099280bfcbfa004
Christian Ramelow <info@codekandis.net>
2019-08-14 14:38:34 +0200

    Adds the `UsersErrorMessages`.

---
+++ src/Errors/UsersErrorMessages.php
@@ -0,0 +1,7 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TradioApi\Errors;
+
+abstract class UsersErrorMessages
+{
+	public const USER_UNKNOWN = 'The requested user does not exist.';
+}
