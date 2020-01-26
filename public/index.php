faa4b1e52f8d0c032152747dd6170a4772b26cb9
Christian Ramelow <info@codekandis.net>
2020-01-26 15:29:26 +0100

    Uses the `SentryClient` integration in the `index.php`.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\TradioApi;
 
+use CodeKandis\SentryClient\SentryClient;
 use CodeKandis\Tiphy\Actions\ActionDispatcher;
+use CodeKandis\TiphySentryClientIntegration\Throwables\Handlers\InternalServerErrorThrowableHandler;
 use CodeKandis\TradioApi\Configurations\ConfigurationRegistry;
 use function error_reporting;
 use function ini_set;
@@ -19,6 +21,13 @@ ini_set( 'html_errors', 'Off' );
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$routesConfiguration = ConfigurationRegistry::_()->getRoutesConfiguration();
-$actionDispatcher    = new ActionDispatcher( $routesConfiguration );
+/** @var ConfigurationRegistry $configurationRegistry */
+$configurationRegistry = ConfigurationRegistry::_();
+
+$sentryClient = new SentryClient( $configurationRegistry->getSentryClientConfiguration() );
+$sentryClient->register();
+
+$routesConfiguration = $configurationRegistry->getRoutesConfiguration();
+$throwableHandler    = new InternalServerErrorThrowableHandler( $sentryClient );
+$actionDispatcher    = new ActionDispatcher( $routesConfiguration, $throwableHandler );
 $actionDispatcher->dispatch();
