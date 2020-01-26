4864b89adaa9e05e1fe188fad85fc3dbce340a3c
Christian Ramelow <info@codekandis.net>
2020-01-26 15:13:46 +0100

    Adds the `SentryClient` configuration.

---
+++ config/sentryClient.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TradioApi\Configurations;
+
+return [
+	'dsn'           => '',
+	'displayErrors' => false,
+	'errorTypes'    => E_ALL,
+	'environment'   => 'development',
+	'release'       => 'dev-development',
+	'serverName'    => 'api.tradio.codekandis'
+];
