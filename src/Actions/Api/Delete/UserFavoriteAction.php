0dede1b7df42742cc38219f2a11b02518c8584a0
Christian Ramelow <info@codekandis.net>
2022-07-20 17:34:48 +0200

    Refactors the `Api\Delete\UserFavoriteAction`.

--- src/Actions/Api/Delete/UserFavoriteAction.php
+++ src/Actions/Api/Delete/UserFavoriteAction.php
@@ -1,13 +1,22 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\TradioApi\Actions\Api\Delete;
 
+use CodeKandis\Persistence\FetchingResultFailedException;
+use CodeKandis\Persistence\InvalidArgumentsStatementsCountException;
+use CodeKandis\Persistence\SettingFetchModeFailedException;
+use CodeKandis\Persistence\StatementExecutionFailedException;
+use CodeKandis\Persistence\StatementPreparationFailedException;
+use CodeKandis\Persistence\TransactionCommitFailedException;
+use CodeKandis\Persistence\TransactionRollbackFailedException;
+use CodeKandis\Persistence\TransactionStartFailedException;
 use CodeKandis\Tiphy\Http\Responses\JsonResponder;
 use CodeKandis\Tiphy\Http\Responses\StatusCodes;
-use CodeKandis\Tiphy\Persistence\PersistenceException;
 use CodeKandis\Tiphy\Throwables\ErrorInformation;
-use CodeKandis\TradioApi\Actions\AbstractWithDatabaseConnectorAction;
+use CodeKandis\TradioApi\Actions\AbstractWithPersistenceConnectorAction;
 use CodeKandis\TradioApi\Entities\FavoriteEntity;
+use CodeKandis\TradioApi\Entities\FavoriteEntityInterface;
 use CodeKandis\TradioApi\Entities\UserEntity;
+use CodeKandis\TradioApi\Entities\UserEntityInterface;
 use CodeKandis\TradioApi\Errors\FavoritesErrorCodes;
 use CodeKandis\TradioApi\Errors\FavoritesErrorMessages;
 use CodeKandis\TradioApi\Errors\UsersErrorCodes;
@@ -15,51 +24,84 @@ use CodeKandis\TradioApi\Errors\UsersErrorMessages;
 use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\FavoritesRepository;
 use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\UsersRepository;
 use JsonException;
+use ReflectionException;
 
-class UserFavoriteAction extends AbstractWithDatabaseConnectorAction
+/**
+ * Represents the action to delete a users' favored track.
+ * @package codekandis/tradio-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class UserFavoriteAction extends AbstractWithPersistenceConnectorAction
 {
 	/**
-	 * @throws PersistenceException
-	 * @throws JsonException
+	 * {@inheritDoc}
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the user entity.
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws TransactionStartFailedException The transaction failed to start.
+	 * @throws TransactionRollbackFailedException The transaction failed to roll back.
+	 * @throws TransactionCommitFailedException The transaction failed to commit.
+	 * @throws InvalidArgumentsStatementsCountException The number of argument lists does not match the number of statements.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
+	 * @throws JsonException An error occurred during the creation of the JSON response.
 	 */
 	public function execute(): void
 	{
 		$inputData = $this->getInputData();
 
-		$requestedUser     = new UserEntity();
-		$requestedUser->id = $inputData[ 'userId' ];
-		$user              = $this->readUserById( $requestedUser );
+		$user = $this->readUserById(
+			UserEntity::fromArray(
+				[
+					'id' => $inputData[ 'userId' ]
+				]
+			)
+		);
 
 		if ( null === $user )
 		{
-			$errorInformation = new ErrorInformation( UsersErrorCodes::USER_UNKNOWN, UsersErrorMessages::USER_UNKNOWN, $inputData );
-			( new JsonResponder( StatusCodes::NOT_FOUND, null, $errorInformation ) )
+			( new JsonResponder(
+				StatusCodes::NOT_FOUND,
+				null,
+				new ErrorInformation( UsersErrorCodes::USER_UNKNOWN, UsersErrorMessages::USER_UNKNOWN, $inputData )
+			) )
 				->respond();
 
 			return;
 		}
 
-		$requestedFavorite     = new FavoriteEntity();
-		$requestedFavorite->id = $inputData[ 'favoriteId' ];
-		$favorite              = $this->readFavoriteById( $requestedFavorite );
+		$favorite = $this->readFavoriteById(
+			FavoriteEntity::fromArray(
+				[
+					'id' => $inputData[ 'favoriteId' ]
+				]
+			)
+		);
 
 		if ( null === $favorite )
 		{
-			$errorInformation = new ErrorInformation( FavoritesErrorCodes::FAVORITE_UNKNOWN, FavoritesErrorMessages::FAVORITE_UNKNOWN, $inputData );
-			( new JsonResponder( StatusCodes::NOT_FOUND, null, $errorInformation ) )
+			( new JsonResponder(
+				StatusCodes::NOT_FOUND,
+				null,
+				new ErrorInformation( FavoritesErrorCodes::FAVORITE_UNKNOWN, FavoritesErrorMessages::FAVORITE_UNKNOWN, $inputData )
+			) )
 				->respond();
 
 			return;
 		}
 
-		$this->deleteFavoriteByUserId( $user, $favorite );
+		$this->deleteFavoriteByUserId( $favorite, $user );
 
 		( new JsonResponder( StatusCodes::OK, null ) )
 			->respond();
 	}
 
 	/**
-	 * @return string[]
+	 * Gets the input data of the request.
+	 * @return string[] The input data of the request.
 	 */
 	private function getInputData(): array
 	{
@@ -67,35 +109,68 @@ class UserFavoriteAction extends AbstractWithDatabaseConnectorAction
 	}
 
 	/**
-	 * @throws PersistenceException
+	 * Reads a user by its ID.
+	 * @param UserEntityInterface $user The user with the ID to search for.
+	 * @return ?UserEntityInterface The user if found, otherwise null.
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the user entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
-	private function readUserById( UserEntity $requestedUser ): ?UserEntity
+	private function readUserById( UserEntityInterface $user ): ?UserEntityInterface
 	{
 		return ( new UsersRepository(
-			$this->getDatabaseConnector()
+			$this->getPersistenceConnector()
 		) )
-			->readUserById( $requestedUser );
+			->readUserById( $user );
 	}
 
 	/**
-	 * @throws PersistenceException
+	 * Reads a favored track by its ID.
+	 * @param FavoriteEntityInterface $favorite The favored track with the ID to search for.
+	 * @return ?FavoriteEntityInterface The favored track if found, otherwise null.
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
-	private function readFavoriteById( FavoriteEntity $requestedFavorite ): ?FavoriteEntity
+	private function readFavoriteById( FavoriteEntityInterface $favorite ): ?FavoriteEntityInterface
 	{
 		return ( new FavoritesRepository(
-			$this->getDatabaseConnector()
+			$this->getPersistenceConnector()
 		) )
-			->readFavoriteById( $requestedFavorite );
+			->readFavoriteById( $favorite );
 	}
 
 	/**
-	 * @throws PersistenceException
+	 * Deletes a favored track by its specific user ID.
+	 * @param FavoriteEntityInterface $favorite The favored track to delete.
+	 * @param UserEntityInterface $user The user with the ID whom the favored track is related with.
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws TransactionStartFailedException The transaction failed to start.
+	 * @throws TransactionRollbackFailedException The transaction failed to roll back.
+	 * @throws TransactionCommitFailedException The transaction failed to commit.
+	 * @throws InvalidArgumentsStatementsCountException The number of argument lists does not match the number of statements.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
 	 */
-	private function deleteFavoriteByUserId( UserEntity $user, FavoriteEntity $favorite ): void
+	private function deleteFavoriteByUserId( FavoriteEntityInterface $favorite, UserEntityInterface $user ): void
 	{
-		( new FavoritesRepository(
-			$this->getDatabaseConnector()
-		) )
-			->deleteFavoriteByUserId( $favorite, $user );
+		$this->getPersistenceConnector()
+			 ->asTransaction(
+				 function () use ( $user, $favorite ): void
+				 {
+					 ( new FavoritesRepository(
+						 $this->getPersistenceConnector()
+					 ) )
+						 ->deleteFavoriteByUserId( $favorite, $user );
+				 }
+			 );
 	}
 }
