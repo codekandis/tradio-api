6ff7fc18ba94c6574815a8afaa047109af3f6ea6
Christian Ramelow <info@codekandis.net>
2022-07-21 00:27:43 +0200

    Refactors the `Api\Get\UserAction`.

--- src/Actions/Api/Get/UserAction.php
+++ src/Actions/Api/Get/UserAction.php
@@ -1,36 +1,59 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\TradioApi\Actions\Api\Get;
 
+use CodeKandis\Persistence\FetchingResultFailedException;
+use CodeKandis\Persistence\SettingFetchModeFailedException;
+use CodeKandis\Persistence\StatementExecutionFailedException;
+use CodeKandis\Persistence\StatementPreparationFailedException;
 use CodeKandis\Tiphy\Http\Responses\JsonResponder;
 use CodeKandis\Tiphy\Http\Responses\StatusCodes;
-use CodeKandis\Tiphy\Persistence\PersistenceException;
 use CodeKandis\Tiphy\Throwables\ErrorInformation;
-use CodeKandis\TradioApi\Actions\AbstractWithDatabaseConnectorAndApiUriBuilderAction;
+use CodeKandis\TradioApi\Actions\AbstractWithPersistenceConnectorAndApiUriBuilderAction;
 use CodeKandis\TradioApi\Entities\UriExtenders\UserApiUriExtender;
 use CodeKandis\TradioApi\Entities\UserEntity;
+use CodeKandis\TradioApi\Entities\UserEntityInterface;
 use CodeKandis\TradioApi\Errors\UsersErrorCodes;
 use CodeKandis\TradioApi\Errors\UsersErrorMessages;
 use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\UsersRepository;
 use JsonException;
+use ReflectionException;
 
-class UserAction extends AbstractWithDatabaseConnectorAndApiUriBuilderAction
+/**
+ * Represents the action to retrieve a specific user.
+ * @package codekandis/tradio-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class UserAction extends AbstractWithPersistenceConnectorAndApiUriBuilderAction
 {
 	/**
-	 * @throws PersistenceException
-	 * @throws JsonException
+	 * {@inheritDoc}
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the user entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
+	 * @throws JsonException An error occurred during the creation of the JSON response.
 	 */
 	public function execute(): void
 	{
 		$inputData = $this->getInputData();
 
-		$requestedUser     = new UserEntity();
-		$requestedUser->id = $inputData[ 'userId' ];
-		$user              = $this->readUserById( $requestedUser );
+		$user = $this->readUserById(
+			UserEntity::fromArray(
+				[
+					'id' => $inputData[ 'userId' ]
+				]
+			)
+		);
 
 		if ( null === $user )
 		{
-			$errorInformation = new ErrorInformation( UsersErrorCodes::USER_UNKNOWN, UsersErrorMessages::USER_UNKNOWN, $inputData );
-			( new JsonResponder( StatusCodes::NOT_FOUND, null, $errorInformation ) )
+			( new JsonResponder(
+				StatusCodes::NOT_FOUND,
+				null,
+				new ErrorInformation( UsersErrorCodes::USER_UNKNOWN, UsersErrorMessages::USER_UNKNOWN, $inputData )
+			) )
 				->respond();
 
 			return;
@@ -38,22 +61,29 @@ class UserAction extends AbstractWithDatabaseConnectorAndApiUriBuilderAction
 
 		$this->extendUris( $user );
 
-		$responderData = [
-			'user' => $user
-		];
-		( new JsonResponder( StatusCodes::OK, $responderData ) )
+		( new JsonResponder(
+			StatusCodes::OK,
+			[
+				'user' => $user
+			]
+		) )
 			->respond();
 	}
 
 	/**
-	 * @return string[]
+	 * Gets the input data of the request.
+	 * @return string[] The input data of the request.
 	 */
 	private function getInputData(): array
 	{
 		return $this->arguments;
 	}
 
-	private function extendUris( UserEntity $user ): void
+	/**
+	 * Extends the URIs of a user.
+	 * @param UserEntityInterface $user The user to extend its URIs.
+	 */
+	private function extendUris( UserEntityInterface $user ): void
 	{
 		( new UserApiUriExtender(
 			$this->getApiUriBuilder(),
@@ -63,13 +93,21 @@ class UserAction extends AbstractWithDatabaseConnectorAndApiUriBuilderAction
 	}
 
 	/**
-	 * @throws PersistenceException
+	 * Reads a user by its ID.
+	 * @param UserEntityInterface $user The user with the ID to search for.
+	 * @return ?UserEntityInterface The user if found, otherwise null.
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the user entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
-	private function readUserById( UserEntity $requestedUser ): ?UserEntity
+	private function readUserById( UserEntityInterface $user ): ?UserEntityInterface
 	{
 		return ( new UsersRepository(
-			$this->getDatabaseConnector()
+			$this->getPersistenceConnector()
 		) )
-			->readUserById( $requestedUser );
+			->readUserById( $user );
 	}
 }
