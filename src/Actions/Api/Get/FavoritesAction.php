f4c89bbe6b12fbdc1da39e513f5231eaaefe5514
Christian Ramelow <info@codekandis.net>
2022-07-20 23:50:39 +0200

    Refactors the `Api\Get\FavoritesAction`.

--- src/Actions/Api/Get/FavoritesAction.php
+++ src/Actions/Api/Get/FavoritesAction.php
@@ -1,37 +1,55 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\TradioApi\Actions\Api\Get;
 
+use CodeKandis\Persistence\FetchingResultFailedException;
+use CodeKandis\Persistence\SettingFetchModeFailedException;
+use CodeKandis\Persistence\StatementExecutionFailedException;
+use CodeKandis\Persistence\StatementPreparationFailedException;
 use CodeKandis\Tiphy\Http\Responses\JsonResponder;
 use CodeKandis\Tiphy\Http\Responses\StatusCodes;
-use CodeKandis\Tiphy\Persistence\PersistenceException;
-use CodeKandis\TradioApi\Actions\AbstractWithDatabaseConnectorAndApiUriBuilderAction;
-use CodeKandis\TradioApi\Entities\FavoriteEntity;
+use CodeKandis\TradioApi\Actions\AbstractWithPersistenceConnectorAndApiUriBuilderAction;
+use CodeKandis\TradioApi\Entities\Collections\FavoriteEntityCollectionInterface;
 use CodeKandis\TradioApi\Entities\UriExtenders\FavoriteApiUriExtender;
 use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\FavoritesRepository;
 use JsonException;
+use ReflectionException;
 
-class FavoritesAction extends AbstractWithDatabaseConnectorAndApiUriBuilderAction
+/**
+ * Represents the action to retrieve all favored tracks.
+ * @package codekandis/tradio-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FavoritesAction extends AbstractWithPersistenceConnectorAndApiUriBuilderAction
 {
 	/**
-	 * @throws PersistenceException
-	 * @throws JsonException
+	 * {@inheritDoc}
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
+	 * @throws JsonException An error occurred during the creation of the JSON response.
 	 */
 	public function execute(): void
 	{
 		$favorites = $this->readFavorites();
 		$this->extendUris( $favorites );
 
-		$responderData = [
-			'favorites' => $favorites,
-		];
-		( new JsonResponder( StatusCodes::OK, $responderData ) )
+		( new JsonResponder(
+			StatusCodes::OK,
+			[
+				'favorites' => $favorites,
+			]
+		) )
 			->respond();
 	}
 
 	/**
-	 * @param FavoriteEntity[] $favorites
+	 * Extends the URIs of a list of favored tracks.
+	 * @param FavoriteEntityCollectionInterface $favorites The favored tracks to extend their URIs.
 	 */
-	private function extendUris( array $favorites ): void
+	private function extendUris( FavoriteEntityCollectionInterface $favorites ): void
 	{
 		$apiUriBuilder = $this->getApiUriBuilder();
 		foreach ( $favorites as $favorite )
@@ -42,13 +60,19 @@ class FavoritesAction extends AbstractWithDatabaseConnectorAndApiUriBuilderActio
 	}
 
 	/**
-	 * @return FavoriteEntity[]
-	 * @throws PersistenceException
+	 * Reads all favored tracks.
+	 * @return FavoriteEntityCollectionInterface The favored tracks.
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
-	private function readFavorites(): array
+	private function readFavorites(): FavoriteEntityCollectionInterface
 	{
 		return ( new FavoritesRepository(
-			$this->getDatabaseConnector()
+			$this->getPersistenceConnector()
 		) )
 			->readFavorites();
 	}
