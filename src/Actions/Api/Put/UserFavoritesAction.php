40195373a7a70decf447f64cfd709cde60984a26
Christian Ramelow <info@codekandis.net>
2022-07-21 12:05:11 +0200

    Refactors the `Api\Put\UserFavoritesAction`.

--- src/Actions/Api/Put/UserFavoritesAction.php
+++ src/Actions/Api/Put/UserFavoritesAction.php
@@ -1,15 +1,22 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\TradioApi\Actions\Api\Put;
 
+use CodeKandis\Persistence\FetchingResultFailedException;
+use CodeKandis\Persistence\InvalidArgumentsStatementsCountException;
+use CodeKandis\Persistence\SettingFetchModeFailedException;
+use CodeKandis\Persistence\StatementExecutionFailedException;
+use CodeKandis\Persistence\StatementPreparationFailedException;
 use CodeKandis\Tiphy\Http\ContentTypes;
 use CodeKandis\Tiphy\Http\Requests\BadRequestException;
 use CodeKandis\Tiphy\Http\Responses\JsonResponder;
 use CodeKandis\Tiphy\Http\Responses\StatusCodes;
-use CodeKandis\Tiphy\Persistence\PersistenceException;
 use CodeKandis\Tiphy\Throwables\ErrorInformation;
-use CodeKandis\TradioApi\Actions\AbstractWithDatabaseConnectorAction;
+use CodeKandis\TradioApi\Actions\AbstractWithPersistenceConnectorAction;
+use CodeKandis\TradioApi\Entities\Collections\FavoriteEntityCollection;
+use CodeKandis\TradioApi\Entities\Collections\FavoriteEntityCollectionInterface;
 use CodeKandis\TradioApi\Entities\FavoriteEntity;
 use CodeKandis\TradioApi\Entities\UserEntity;
+use CodeKandis\TradioApi\Entities\UserEntityInterface;
 use CodeKandis\TradioApi\Errors\CommonErrorCodes;
 use CodeKandis\TradioApi\Errors\CommonErrorMessages;
 use CodeKandis\TradioApi\Errors\UsersErrorCodes;
@@ -18,15 +25,28 @@ use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\FavoritesRepository;
 use CodeKandis\TradioApi\Persistence\MariaDb\Repositories\UsersRepository;
 use JsonException;
 use ReflectionException;
+use stdClass;
+use function array_map;
 use function is_object;
 use function strtolower;
 
-class UserFavoritesAction extends AbstractWithDatabaseConnectorAction
+class UserFavoritesAction extends AbstractWithPersistenceConnectorAction
 {
 	/**
-	 * @throws PersistenceException
-	 * @throws ReflectionException
-	 * @throws JsonException
+	 * {@inheritDoc}
+	 * @throws BadRequestException The content type is invalid.
+	 * @throws BadRequestException The request body is malformed.
+	 * @throws BadRequestException The request body is invalid.
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the user entity.
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
+	 * @throws InvalidArgumentsStatementsCountException The number of argument lists does not match the number of statements.
+	 * @throws JsonException An error occurred during the creation of the JSON response.
 	 */
 	public function execute(): void
 	{
@@ -36,41 +56,59 @@ class UserFavoritesAction extends AbstractWithDatabaseConnectorAction
 		}
 		catch ( BadRequestException $exception )
 		{
-			$errorInformation = new ErrorInformation( $exception->getCode(), $exception->getMessage() );
-			( new JsonResponder( StatusCodes::BAD_REQUEST, null, $errorInformation ) )
+			( new JsonResponder(
+				StatusCodes::BAD_REQUEST,
+				null,
+				new ErrorInformation( $exception->getCode(), $exception->getMessage() )
+			) )
 				->respond();
 
 			return;
 		}
 
-		$requestedUser     = new UserEntity();
-		$requestedUser->id = $inputData[ 'userId' ];
-		$user              = $this->readUser( $requestedUser );
+		$user = $this->readUser(
+			UserEntity::fromArray(
+				[
+					'id' => $inputData[ 'userId' ]
+				]
+			)
+		);
 
 		if ( null === $user )
 		{
-			$errorInformation = new ErrorInformation( UsersErrorCodes::USER_UNKNOWN, UsersErrorMessages::USER_UNKNOWN );
-			( new JsonResponder( StatusCodes::BAD_REQUEST, null, $errorInformation ) )
+			( new JsonResponder(
+				StatusCodes::BAD_REQUEST,
+				null,
+				new ErrorInformation( UsersErrorCodes::USER_UNKNOWN, UsersErrorMessages::USER_UNKNOWN )
+			) )
 				->respond();
 
 			return;
 		}
 
-		foreach ( $inputData[ 'favorites' ] as $sentFavorite )
-		{
-			/**
-			 * @var FavoriteEntity $favorite
-			 */
-			$favorite = FavoriteEntity::fromObject( $sentFavorite );
-			$this->writeFavoriteByUserId( $favorite, $user );
-		}
+		$this->createFavoritesByUserId(
+			new FavoriteEntityCollection(
+				...array_map(
+					function ( stdClass $favorite )
+					{
+						return FavoriteEntity::fromObject( $favorite );
+					},
+					$inputData[ 'favorites' ]
+				)
+			),
+			$user
+		);
 
 		( new JsonResponder( StatusCodes::OK, null ) )
 			->respond();
 	}
 
 	/**
-	 * @throws BadRequestException
+	 * Gets the input data of the request.
+	 * @return array The input data of the request.
+	 * @throws BadRequestException The content type is invalid.
+	 * @throws BadRequestException The request body is malformed.
+	 * @throws BadRequestException The request body is invalid.
 	 */
 	private function getInputData(): array
 	{
@@ -112,24 +150,49 @@ class UserFavoritesAction extends AbstractWithDatabaseConnectorAction
 	}
 
 	/**
-	 * @throws PersistenceException
+	 * Reads a user by its ID.
+	 * @param UserEntityInterface $user The user with the ID to search for.
+	 * @return ?UserEntityInterface The user if found, otherwise null.
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the user entity.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
-	private function readUser( UserEntity $requestedUser ): ?UserEntity
+	private function readUser( UserEntityInterface $user ): ?UserEntityInterface
 	{
 		return ( new UsersRepository(
-			$this->getDatabaseConnector()
+			$this->getPersistenceConnector()
 		) )
-			->readUserById( $requestedUser );
+			->readUserById( $user );
 	}
 
 	/**
-	 * @throws PersistenceException
+	 * Creates a favorite track for a specific user.
+	 * @param FavoriteEntityCollectionInterface $favorites The favorite tracks to create.
+	 * @param UserEntityInterface $user The user with the ID whom the favorite track is related with.
+	 * @throws ReflectionException The favorite track entity class to reflect does not exist.
+	 * @throws ReflectionException An error occurred during the creation of the favorite track entity.
+	 * @throws ReflectionException The user entity class to reflect does not exist.
+	 * @throws InvalidArgumentsStatementsCountException The number of argument lists does not match the number of statements.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
 	 */
-	private function writeFavoriteByUserId( FavoriteEntity $favorite, UserEntity $user ): void
+	private function createFavoritesByUserId( FavoriteEntityCollectionInterface $favorites, UserEntityInterface $user ): void
 	{
-		( new FavoritesRepository(
-			$this->getDatabaseConnector()
-		) )
-			->writeFavoriteByUserId( $favorite, $user );
+		$this->getPersistenceConnector()
+			 ->asTransaction(
+				 function () use ( $favorites, $user )
+				 {
+					 $favoriteRepository = new FavoritesRepository(
+						 $this->getPersistenceConnector()
+					 );
+					 foreach ( $favorites as $favorite )
+					 {
+						 $favoriteRepository->createFavoriteByUserId( $favorite, $user );
+					 }
+				 }
+			 );
 	}
 }
