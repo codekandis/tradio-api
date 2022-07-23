e510a98787815f82df2c7ead36f512df24c2d2b5
Christian Ramelow <info@codekandis.net>
2022-07-23 13:01:26 +0200

    Updates the release version to `0.9.0`.

--- src/Configurations/Plain/sentryClient.php
+++ src/Configurations/Plain/sentryClient.php
@@ -8,6 +8,6 @@ return [
 	'displayErrors' => false,
 	'errorTypes'    => E_ALL,
 	'environment'   => 'production',
-	'release'       => '0.8.0',
+	'release'       => '0.9.0',
 	'serverName'    => 'api.tradio.codekandis'
 ];
